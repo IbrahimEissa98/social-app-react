bfd038440d19ceb0797b7f4f61f5baa8aed74a84
IbrahimEissa98 <ibrahimqutb112@gmail.com>
2025-08-20 19:12:20 +0300

    update bg

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
+  const bgImage = "../../public/low-poly-grid-haikei.svg";
   return (
     <>
       <div
-        className={`flex justify-center py-16 min-h-screen bg-[url(../../public/low-poly-grid-haikei.svg)] bg-no-repeat bg-cover`}
+        className={`flex justify-center py-16 min-h-screen bg-[url(${bgImage})] bg-no-repeat bg-cover`}
       >
         <Outlet />
       </div>
