c69f78a2d1c6cad0f5ae9f9782590abfd589a8e5
IbrahimEissa98 <ibrahimqutb112@gmail.com>
2025-08-21 04:54:08 +0300

    bg image

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import bgImage from "../assets/images/low-poly-grid-haikei.svg";
+// import bgImage from "../assets/images/low-poly-grid-haikei.svg";
 
 export default function AuthLayout() {
+  const bgImageUrl = "/public/low-poly-grid-haikei.svg"; // Adjust the path as needed
   return (
     <>
       <div
-        className={`flex justify-center py-16 min-h-screen bg-[url(${bgImage})] bg-no-repeat bg-cover`}
+        className={`flex justify-center py-16 min-h-screen bg-[url(${bgImageUrl})] bg-no-repeat bg-cover`}
       >
         <Outlet />
       </div>
